b21def55b06fe3db4099f3807ff59342ce82a396
DinoSilva <51093627+DinoSilva@users.noreply.github.com>
2021-03-01 21:59:24 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,9 +1,9 @@
 # BTC node configurations
 
-rpc_user = "mynode"
-rpc_passwd = "9k35rrienAmBVfGotHcf6J6c"
+rpc_user = "XXXXXX"
+rpc_passwd = "xyz"
 
-node_ip = "192.168.1.65"
+node_ip = "00.00.00.00"
 node_rpc_port = "8332"
 
 node_main_net = "http://%s:%s@%s:%s" % (rpc_user, rpc_passwd, node_ip, node_rpc_port)
