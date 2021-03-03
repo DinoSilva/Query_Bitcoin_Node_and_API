9fae904dad44f1ae75c3b5b614831c02a99a1e15
DinoSilva <51093627+DinoSilva@users.noreply.github.com>
2021-03-03 20:57:27 +0000

    Add files via upload

--- Node_Build_Block_Table.py
+++ Node_Build_Block_Table.py
@@ -13,7 +13,9 @@ import time
 # https://github.com/jgarzik/python-bitcoinrpc
 from bitcoinrpc.authproxy import AuthServiceProxy
 
-from openpyxl import load_workbook
+import sqlite3
+# import pyodbc
+
 
 rpc_connection = AuthServiceProxy(node_main_net, timeout=120)
 
@@ -22,23 +24,38 @@ items = []
 start_time = time.time()
 
 # select the required blocks in the range function
-for i in range(100, 1000):
+for i in range(1, 1000):
     block_time = time.time()
 
-    # TODO add exception handling for connectivity errors
+    # TODO add exception hasndling for connectivity errors
     block_hash = rpc_connection.getblockhash(i)
     block_raw = rpc_connection.getblockheader(block_hash)
 
-    # items = [value for value in block_raw.values()]
-    items.append([value for value in block_raw.values()])
+    t_block = tuple(value for value in block_raw.values())
+
+    # to extract table headers
+    # n_block = tuple(key for key in block_raw.keys())
+    items.append(t_block)
 
     print("\nBlock ", i, " time - %.0f ms" % ((time.time() - block_time) * 1000))
 
 # TODO exception handling for file errors
-workbook = load_workbook(filename=xls_name)
-sheet = workbook.active
+conn = sqlite3.connect(sql_file)
+cursor = conn.cursor()
+""" TO CREATE TABLE
+cursor.execute('''CREATE TABLE Block_headers 
+             ('hash', 'confirmations', 'height', 'version', 'versionHex', 'merkleroot', 'time', 'mediantime', 'nonce', 'bits', 'difficulty', 'chainwork', 'nTx', 'previousblockhash', 'nextblockhash')''')
+"""
+# FIXME 1st block cannot be inserted, there
+
 for item in items:
-    sheet.append(item)
-workbook.save(filename=xls_name)
+    print("INSERT INTO Block_headers VALUES "+str(item))
+    cursor.execute("INSERT INTO Block_headers VALUES "+str(item))
+
+
+for row in cursor.execute('SELECT * FROM Block_headers'):
+    print(row)
+
 
 print("\nTotal time --- %.0d seconds" % (time.time() - start_time))
+
